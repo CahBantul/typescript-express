ea3fd798a73f4f127a4430895ad1c974cd1956f3
fardan <fardan.nozami@gmail.com>
2022-08-05 22:08:37 +0700

    add body parser and morgan

--- build/index.js
+++ build/index.js
@@ -4,14 +4,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
 class App {
     constructor() {
         this.app = (0, express_1.default)();
+        this.middleware();
         this.routes();
+    }
+    middleware() {
+        this.app.use(express_1.default.json());
+        this.app.use(express_1.default.urlencoded({ extended: true }));
+        this.app.use((0, morgan_1.default)('dev'));
     }
     routes() {
         this.app.route('/').get((req, res) => {
             res.json('app from TS');
+        });
+        this.app.route('/').post((req, res) => {
+            res.json(req.body);
         });
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,27 @@
 import express, { Application, Request, Response } from 'express';
+import morgan from 'morgan';
 
 class App {
   public app: Application;
 
   constructor() {
     this.app = express();
+    this.middleware();
     this.routes();
+  }
+
+  protected middleware(): void {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(morgan('dev'));
   }
 
   protected routes(): void {
     this.app.route('/').get((req: Request, res: Response) => {
       res.json('app from TS');
+    });
+    this.app.route('/').post((req: Request, res: Response) => {
+      res.json(req.body);
     });
   }
 }
